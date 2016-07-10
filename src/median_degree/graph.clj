536fa7f567aa04460b6a7c73bcb627a647dbc7fe
kalman <kal.chapman@gmail.com>
2016-07-10 23:23:33 +0000

    init commit - manages graph of user interactions

---
+++ src/median_degree/graph.clj
@@ -0,0 +1,102 @@
+(ns median-degree.graph
+  "namespace manages graph of venmo transactions
+  stores graph in atomic vector of median-degree.schema/VenmoTX structs.
+  this gives a complete description of graph with directionality and 
+  additional data to drive edge eviction.
+  Graph mutation operations are perfomed on edge-list representation
+  as our main focus is adding edges and evicting them based on edge metadata
+  edges are stored as sets of node identities that serve as keys to edge metadata"
+  (:require [clj-time.core :as time]
+            [clj-time.coerce :as ctime]
+            [clojure.core.reducers :as r]))
+
+(def venmo-graph
+  "initializes graph as empty vector on require"
+  (atom {}))
+
+(def latest-transaction
+  "initializes as empty transaction on require"
+  (atom (ctime/from-long 0)))
+
+(defn compare-transaction-times
+  "compares transaction times"
+  [a b]
+  (if (time/after? a  b)
+    a b))
+
+(defn find-latest-transaction
+  "expects a vector of median-degree.schema/VenmoTX structs,
+  reduces to the latest tx"
+  [graph]
+  (reduce compare-transaction-times (vals graph)))
+
+(defn eviction-limit
+  "provides boolean predicate on whether a datetime falls sooner than
+  60 seconds of a given datetime"
+  [target limit]
+  #(time/after? % (time/minus target limit))) 
+
+(defn evict-edges
+  "takes existing graph & new transaction,
+  evicts outdated edges and returns valid graph"
+  ([graph]
+   (let [latest-created (find-latest-transaction graph)]
+     (evict-edges graph latest-created)))
+  ([graph latest-created]
+     (into {} (filter 
+                (comp (eviction-limit latest-created (time/seconds 60))
+                      val) graph))))
+
+(defn merge-into-graph
+  "merges edge into graph - replaces value if created time is newer
+  guarantees only one, undirected edge in graph for any node pair"
+  [graph edge]
+  (merge-with compare-transaction-times graph edge))
+
+(defn form-edge-from-tx
+  "forms an edge from a tx object, where an edge consists of a hashmap:
+  {#{actor target} time_created}
+  using a set of node ids as this graph is undirected"
+  [tx]
+  {(set ((juxt :actor :target) tx)) (:created_time tx)})
+
+(def latest-transaction!
+  "updates compares and updates the latest transaction information
+  given a new edge"
+  (partial swap! latest-transaction compare-transaction-times))
+
+(defn add-edge
+  "takes a median-degree.schema/VenmoTX struct and upserts into graph
+  evicts edges and returns valid edge list of tx graph"
+  ([graph new-edge]
+   (evict-edges (merge-into-graph graph new-edge)))
+  ([graph new-edge latest-transaction]
+   (evict-edges (merge-into-graph graph new-edge) latest-transaction)))
+
+(def add-edge!
+  "swaps atomic value of venmo-graph
+ with update per new edge"
+ (partial swap! venmo-graph add-edge)) 
+
+(defn fetch-edges
+  "fetches edges of graph from atomic graph unless
+  graph is provided as arg"
+  ([]
+   (fetch-edges @venmo-graph))
+  ([graph]
+   (keys graph)))
+
+(defn fetch-nodes
+  "fetches a set of node ids
+  grabs from atomic graph unless graph is provided as arg"
+  ([]
+   (fetch-nodes @venmo-graph))
+  ([graph]
+   (reduce clojure.set/union (fetch-edges graph))))
+
+(defn fetch-node-degrees
+  "returns node degrees for atomic graph unless graph is specified"
+  ([]
+   (fetch-node-degrees @venmo-graph))
+  ([graph]
+   (frequencies (flatten (map #(vec %) (fetch-edges graph))))))
