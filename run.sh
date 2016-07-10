3e0a5d6e38160bea91c3b516c7bea16f441ee45c
kalman <kal.chapman@gmail.com>
2016-07-10 23:22:47 +0000

    initial changes to get run.sh to run with lein

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@
 # but could be replaced with similar files from any major language
 
 # I'll execute my programs, with the input directory venmo_input and output the files in the directory venmo_output
-python ./src/rolling_median.py ./venmo_input/venmo-trans.txt ./venmo_output/output.txt
-
+#python ./src/rolling_median.py ./venmo_input/venmo-trans.txt ./venmo_output/output.txt
+lein run -i ./venmo_input/venmo-trans.txt -o ./venmo_output/output.txt
 
 
