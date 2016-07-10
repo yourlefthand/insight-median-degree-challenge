e5ef32e3d57c7bfd3582e110cab2d7615b975667
kalman <kal.chapman@gmail.com>
2016-07-10 00:45:36 +0000

    includes source file for transaction schema & validation

---
+++ src/median_degree/schema.clj
@@ -0,0 +1,41 @@
+(ns median-degree.schema
+  (:require [schema.core :as s]
+            [schema.coerce :as c]
+            [schema.utils :as s-utils]
+            [clj-time.core :as time]
+            [clj-time.coerce :as ctime])
+  (:import org.joda.time.DateTime))
+
+(s/defschema VenmoTX
+  "schema for coercing 
+   and validating venmo transactions"
+  {:target s/Str
+   :actor s/Str
+   :created_time DateTime})
+
+(defn date-matcher
+  "provides coercion from string to date"
+  [schema]
+  (when (= DateTime schema)
+    (c/safe
+      (fn [x]
+        (if (string? x) (ctime/from-string x) x)))))
+
+(def tx-matcher
+  "provides string + json coercion"
+  (c/first-matcher [date-matcher c/json-coercion-matcher]))
+
+(defn coerce-and-validate
+  "generic coercion/validation with throw"
+  [schema matcher data]
+  (let [coercer (c/coercer schema matcher)
+        result (coercer data)]
+    (if (s-utils/error? result)
+      (throw (Exception. (format "Value does not match schema :%s"
+                                 (s-utils/error-val result))))
+      result)))
+
+(defn tx-validate
+  "coercion/validation for VenmoTX"
+  [data]
+  (coerce-and-validate VenmoTX tx-matcher data))
