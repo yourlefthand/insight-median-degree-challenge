3819632dfb74103b2d2d171ed4cf5071a7c5b4c7
kalman <kalchapman@gmail.com>
2016-07-11 14:12:39 -0400

    simplifies since testsuite rebuilds the project anyway

--- run.sh
+++ run.sh
@@ -1,9 +1,4 @@
 #!/usr/bin/env bash
 
-##changes working dir to src, then runs lein to initialize main method
-
-if [ ! -f ./src/target/uberjar/median-degree-0.1.0-standalone.jar ]; then
-	(cd ./src && lein uberjar)
-fi
-
-java -jar ./src/target/uberjar/median-degree-0.1.0-standalone.jar -i ./venmo_input/venmo-trans.txt -o ./venmo_output/output.txt
+##dips into ./src to build uberjar, then runs with arguments to play nice with insight testsuite
+(cd ./src && lein uberjar) && java -jar ./src/target/uberjar/median-degree-0.1.0-standalone.jar -i ./venmo_input/venmo-trans.txt -o ./venmo_output/output.txt
